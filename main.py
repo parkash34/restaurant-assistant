f21cec80d1ad6a27b9553b7dbd2bdef15d834e7d
parkash34 <ohm.parkash34@gmail.com>
2026-03-26 14:53:57 +0100

    Updated it

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 from dotenv import load_dotenv
 from pydantic import BaseModel
 import json
+from datetime import date
 
 load_dotenv()
 
@@ -20,50 +21,64 @@ class Message(BaseModel):
     customer_name : str
     message : str
 
-name = "Parkash"
-memebership = "VIP"
 
-def build_system_prompt(customer_name, membership):
+def build_system_prompt(customer_name):
+    today_date = date.today().strftime("%Y-%m-%d")
+    today_special = "Revolutional Day"
+    available_tables = "Tables 2, 5 and 8"
     return f""" 
     You are Muzo, a restaurant assistant for Pizza Hut. 
+
+    Current Context:
+    - Today's Date : {today_date}
+    - Today's Special : {today_special}
+    - Available Tables : {available_tables}
+    - Customer Name : {customer_name}
+
     Your goal is to answer questions of customers related to Pizza Hut,
     menu, reservations, and timing. Always be polite, calm, and friendly,
     and keep responses clear and concise. If any unrelated question is asked,
     don't answer them and make them get to conversation.
+
     Always respond in this JSON format:
-    { 
+    {{
         "category": "Menu or Reservation or Complaint or other", 
         "answer": "your answer here", "follow_up": 
         "a follow-up question if needed." 
-    }
+    }}
     Do not add any extra text outside the JSON.
+
     Examples: 
+
     User: Do you have spicy pizza? 
-    Response: { 
+    Response: {{
         "category": "Menu",
         "answer": "Yes, we have spicy pizza with fresh tomatoes and chicken!",
         "follow_up": "Would you like medium or extra spicy?" 
-    }
+    }}
+
     User: I want to book a table for 8. 
-    Response: { 
+    Response: {{
         "category": "Reservation",
-        "answer": "I can help with that!",
+        "answer" : "I can help with that! We currently have {available_tables} open.",
         "follow_up": "What date and time works for you?" 
-    }
+    }}
+
     User: Your pizza was 20 minutes late.
-    Response:{ 
+    Response:{{
         "category": "Complaint",
         "answer": "Thanks for coming to Pizza Hut. We're very sorry to hear that. It's Resolution Day, and it's a bit crowded. We will try to make it better; you don't have to face that problem again.",
         "follow_up": "Is there anything else we can help you with?"
-    }
+    }}
+
     User: What's the capital of Paris?
-    Response:{ 
+    Response:{{
         "category": "Other", 
         "answer": "Sorry, I cannot answer that. I can help you with questions related to our restaurant, or Menu or reservation ", 
         "follow_up": "Do you want to ask anything related to our restaurant or our Menu or reservation?"
-    }
+    }}
 """
-def ask_ai(chat_history):
+def ask_ai(chat_history, customer_name):
     try:
         response = requests.post(
             "https://api.groq.com/openai/v1/chat/completions",
@@ -72,14 +87,16 @@ def ask_ai(chat_history):
                 "model": "llama-3.3-70b-versatile",
                 "temperature": 0.3,
                 "max_tokens": 500,
-                "message": [
-                    {"role": "system", "content": f"{build_system_prompt(name, memebership)}"},
+                "messages": [
+                    {"role": "system", "content": f"{build_system_prompt(customer_name)}"},
 
                     *chat_history
                 ]
             },
             timeout=10
         )
+
+        response.raise_for_status()
         raw = response.json()["choices"][0]["message"]["content"]
         return json.loads(raw)
     except requests.exceptions.Timeout:
@@ -92,7 +109,7 @@ def ask_ai(chat_history):
         return f"Something went wrong: {str(e)}"
 
 
-@app.post("/restaurant-assistant")
+@app.post("/chat")
 def restaurant_chat(messages: Message):
     if not messages.session_id:
         return {"error" : "Session ID is missing"}
@@ -101,13 +118,15 @@ def restaurant_chat(messages: Message):
     
     session_id = messages.session_id
     user_message = messages.message
+    customer_name = messages.customer_name
 
     if session_id not in memory:
         memory[session_id] = [] 
 
     memory[session_id].append({"role":"user", "content": user_message})
-    ai_reply = ask_ai(memory[session_id])
+    ai_reply = ask_ai(memory[session_id], customer_name)
 
-    memory[session_id].append({"role":"assistant", "content": ai_reply})
-    return {"reply": ai_reply}
+    if "error" not in ai_reply.get("category","").lower():
+        memory[session_id].append({"role":"assistant", "content": json.dumps(ai_reply)})
+    return ai_reply
 
